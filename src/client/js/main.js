bd3eb141055182d98fc65f96386900c69c80f760
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-05-09 21:16:57 +0900

    feat: Create App component

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,5 +1,42 @@
 import "../scss/styles.scss";
+import Home from "./components/Home.js";
+import Component from "./core/Component.js";
+import { $ } from "./utils/dom.js";
 
-const $app = document.querySelector("#app");
+class App extends Component {
+  initRouter() {
+    this.router = {
+      "/": Home,
+    };
+    window.addEventListener("popstate", () => {
+      this.render();
+    });
+    window.dispatchEvent(new Event("popstate"));
+  }
+  template() {
+    return `
+      <main></main>
+      <div id="music-player"></div>
+    `;
+  }
+  addComponent() {
+    const path = window.location.pathname;
+    const pageComponent = this.router[path];
+    new pageComponent($("main"));
+  }
 
-$app.innerHTML = "<h1>Hello, world!</h1>";
+  setEvent() {
+    const links = document.querySelectorAll("a[href]");
+    links.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const path = link.pathname;
+        window.history.pushState(null, "", path);
+        const pageComponent = this.router[path];
+        new pageComponent($("main"));
+      });
+    });
+  }
+}
+
+new App($("#app"));
