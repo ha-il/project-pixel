fd1b28dd8078390613efc0daa92abf195d377829
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-05-10 15:58:53 +0900

    feat: Create Modal component and add it to Home component

--- src/client/js/components/Home.js
+++ src/client/js/components/Home.js
@@ -1,19 +1,38 @@
 import Component from "../core/Component.js";
+import { $ } from "../utils/dom.js";
+import Modal from "./Modal.js";
 
 class Home extends Component {
   template() {
     return `
     <div class="home-container">
-      <div id="window">창문</div>
-      <div id="bed">침대</div>
-      <div id="cabinet">보관함</div>
-      <div id="tv">TV</div>
-      <div id="monitor">모니터</div>
-      <div id="desk">책상</div>
-      <div id="coffee">커피</div>
-      <div id="chair">의자</div>
+      <div id="window" class="object">창문</div>
+      <div id="bed" class="object">침대</div>
+      <div id="cabinet" class="object">보관함</div>
+      <div id="tv" class="object">TV</div>
+      <div id="monitor" class="object">모니터</div>
+      <div id="desk" class="object">책상</div>
+      <div id="coffee" class="object">커피</div>
+      <div id="chair" class="object">의자</div>
+      <div id="modal" class="object hidden"></div>
     </div>
     `;
+  }
+
+  setEvent() {
+    $(".home-container").addEventListener("click", (e) => {
+      if (e.target.id === "monitor") {
+        window.history.pushState(null, "", "/login");
+        $("#modal").classList.remove("hidden");
+        new Modal($("#modal"));
+      }
+    });
+    window.addEventListener("DOMContentLoaded", (event) => {
+      if (window.location.pathname === "/login") {
+        $("#modal").classList.remove("hidden");
+        new Modal($("#modal"));
+      }
+    });
   }
 }
 
---
+++ src/client/js/components/Modal.js
@@ -0,0 +1,26 @@
+import Component from "../core/Component.js";
+import { $ } from "../utils/dom.js";
+import Login from "./modals/Login.js";
+
+class Modal extends Component {
+  template() {
+    return `
+      <button type="button" class="close-button">X</button>
+      <div class="modal-content">컨텐츠</div>
+    `;
+  }
+  addComponent() {
+    if (window.location.pathname === "/login") {
+      new Login($(".modal-content"));
+    }
+  }
+
+  setEvent() {
+    $(".close-button").addEventListener("click", () => {
+      this.$target.classList.add("hidden");
+      window.history.pushState(null, "", "/");
+    });
+  }
+}
+
+export default Modal;
