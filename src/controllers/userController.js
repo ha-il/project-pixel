be8db5748ff166f6f69aa158a90ff5f407275380
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-05-12 16:51:46 +0900

    feat: Add login router to apiRouter.js and Create login controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 export const createAccount = async (req, res) => {
   const { username, profileName, password, passwordConfirm } = req.body;
@@ -23,4 +24,30 @@ export const createAccount = async (req, res) => {
   } catch (error) {
     return res.sendStatus(400);
   }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(400).send({
+      errorMessage: `${username}로 가입한 유저가 존재하지 않습니다.`,
+    });
+  }
+  const hashedPassword = user.password;
+  const isValidPassword = await bcrypt.compare(password, hashedPassword);
+  if (!isValidPassword) {
+    return res.status(400).send({
+      errorMessage: `비밀번호가 일치하지 않습니다.`,
+    });
+  }
+
+  req.session.isLoggedIn = true;
+  req.session.loggedInUser = user;
+
+  res.cookie("isLoggedIn", true);
+  res.cookie("loggedInUser", JSON.stringify(user));
+
+  return res.sendStatus(200);
 };
--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createAccount } from "../controllers/userController.js";
+import { createAccount, login } from "../controllers/userController.js";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/users/signup", createAccount);
+apiRouter.post("/users/login", login);
 
 export default apiRouter;
