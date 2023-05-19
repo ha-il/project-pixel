53c961bf245047157441220fa860df382614cdc1
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-05-19 20:47:44 +0900

    feat: Create PlaylistCreation component

---
+++ src/client/js/components/phone/PlaylistCreation.js
@@ -0,0 +1,74 @@
+import Component from "../../core/Component.js";
+import { $ } from "../../utils/dom.js";
+import PhoneHome from "./PhoneHome.js";
+
+class PlaylistCreation extends Component {
+  template() {
+    return `
+      <button type="button" class="back-button">↩</button>
+      <div class="phone-form-container">
+        <form method="post" id="playlist-creation-form">
+          <div class="form-input">
+            <label for="name">플레이리스트 이름</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="플레이리스트 이름을 입력해주세요"
+              maxlength="30"
+              required
+            />
+          </div>
+          <div class="form-input">
+            <label for="description">플레이리스트 설명</label>
+            <input
+              id="description"
+              name="description"
+              type="text"
+              placeholder="플레이리스트 설명을 입력해주세요"
+              maxlength="50"
+              required
+            />
+          </div>
+          <input type="submit" , value="플레이리스트 생성" />
+          <div id="playlist-creation-error" class="error_message"></div>
+        </form>
+      </div>
+    `;
+  }
+  setEvent() {
+    $(".back-button").addEventListener("click", () => {
+      new PhoneHome(this.$target);
+    });
+    $("#playlist-creation-form").addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const userInputData = JSON.stringify({
+        name: $("#name").value,
+        description: $("#description").value,
+      });
+
+      const response = await fetch(`/api/playlists`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: userInputData,
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        const { initApp, $app } = this.props;
+        window.history.pushState(null, "", `/playlists/${data.playlistId}`);
+        return initApp($app);
+      } else {
+        return ($(
+          "#playlist-creation-error"
+        ).innerText = `${data.errorMessage}`);
+      }
+    });
+  }
+}
+
+export default PlaylistCreation;
