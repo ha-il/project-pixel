05b3f6e03f7b14cfdc1fe0575393d0b6f337f59c
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-05-19 16:48:10 +0900

    feat: Add api router about music registration

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { createAccount, login, logout } from "../controllers/userController.js";
+import { getMusicInfoFromYoutube } from "../controllers/youtubeApiController.js";
+import { registerMusic } from "../controllers/musicController.js";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/users/signup", createAccount);
 apiRouter.post("/users/login", login);
 apiRouter.post("/users/logout", logout);
+
+apiRouter.get("/youtube/musics/:music_id", getMusicInfoFromYoutube);
+apiRouter.post("/musics/music", registerMusic);
 
 export default apiRouter;
