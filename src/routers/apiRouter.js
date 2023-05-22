87973db56fe85877f1f321f691dbd4926cce409c
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-05-22 21:34:37 +0900

    feat: Add GET chart musics, GET playlist, POST add music to playlist api routers

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { createAccount, login, logout } from "../controllers/userController.js";
 import { getMusicInfoFromYoutube } from "../controllers/youtubeApiController.js";
-import { registerMusic } from "../controllers/musicController.js";
-import { createPlaylist } from "../controllers/playlistController.js";
+import {
+  getChartMusics,
+  registerMusic,
+} from "../controllers/musicController.js";
+import {
+  addMusicToPlaylist,
+  createPlaylist,
+  getPlaylist,
+} from "../controllers/playlistController.js";
 
 const apiRouter = express.Router();
 
@@ -13,7 +20,10 @@ apiRouter.post("/users/logout", logout);
 apiRouter.get("/youtube/musics/:music_id", getMusicInfoFromYoutube);
 
 apiRouter.post("/musics", registerMusic);
+apiRouter.get("/musics/chart", getChartMusics);
 
 apiRouter.post("/playlists", createPlaylist);
+apiRouter.get("/playlists/:playlistId", getPlaylist);
+apiRouter.post("/playlists/:playlistId", addMusicToPlaylist);
 
 export default apiRouter;
