9129991527cc2099239833a0a400eb647e709108
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-05-22 21:31:51 +0900

    feat: Create getPlaylist and addMusicToPlaylist controllers

--- src/controllers/playlistController.js
+++ src/controllers/playlistController.js
@@ -1,3 +1,4 @@
+import Music from "../models/Music.js";
 import Playlist from "../models/Playlist.js";
 
 export const createPlaylist = async (req, res) => {
@@ -14,5 +15,52 @@ export const createPlaylist = async (req, res) => {
     return res.status(400).send({
       errorMessage: `플레이리스트를 생성하는 과정에서 오류가 발생했습니다.`,
     });
+  }
+};
+
+export const getPlaylist = async (req, res) => {
+  const { playlistId } = req.params;
+
+  try {
+    const playlist = await Playlist.findById(playlistId)
+      .populate("owner")
+      .populate("musics");
+
+    if (!playlist) {
+      return res
+        .status(404)
+        .send({ errorMessage: "플레이리스트를 찾을 수 없었습니다." });
+    }
+
+    return res.status(200).send(playlist);
+  } catch (error) {
+    return res.status(400).send({
+      errorMessage: "플레이리스트를 조회하는 과정에서 에러가 발생했습니다.",
+    });
+  }
+};
+
+export const addMusicToPlaylist = async (req, res) => {
+  const {
+    params: { playlistId },
+    body: { musicId },
+  } = req;
+  try {
+    const music = await Music.findById(musicId);
+    const playlist = await Playlist.findByIdAndUpdate(
+      playlistId,
+      {
+        $push: { musics: music },
+      },
+      { new: true }
+    )
+      .populate("owner")
+      .populate("musics");
+
+    return res.status(200).send(playlist);
+  } catch (error) {
+    return res
+      .status(400)
+      .send({ errorMessage: "곡을 추가하는 과정에서 에러가 발생했습니다." });
   }
 };
