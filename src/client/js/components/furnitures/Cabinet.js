a6bc513f252e4e81ea9489bb512364e42df5e189
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-05-26 18:33:44 +0900

    fix: Update initialization of Cabinet component

--- src/client/js/components/furnitures/Cabinet.js
+++ src/client/js/components/furnitures/Cabinet.js
@@ -3,7 +3,14 @@ import { getCookie } from "../../utils/cookie.js";
 import Playlist from "../Playlist.js";
 
 class Cabinet extends Component {
+  init() {
+    this.state = this.initState();
+    this.initRouter();
+    this.fetchData();
+    this.render();
+  }
   async fetchData() {
+    if (!getCookie("isLoggedIn")) return;
     const userId = getCookie("loggedInUser")._id;
     const response = await fetch(`/api/users/playlists/${userId}`);
 
@@ -13,25 +20,70 @@ class Cabinet extends Component {
     }
   }
   template() {
-    const arr = this.state.playlists
-      ? [1, 2, 3, ...this.state.playlists]
-      : [1, 2, 3];
+    const arr = this.state.playlists ? [...this.state.playlists] : [];
     return `
+      <div class="cabinet-container">
+        ${arr
+          .slice(0, 3)
+          .map((item, i) => {
+            return `<a class="playlist" data-playlistid=${item}>
+                      <img src=../../../../../images/playlist-${i}.png />
+                    </a>`;
+          })
+          .join("")} 
+      </div>
+      <div class="cabinet-container">
       ${arr
+        .slice(3, 6)
         .map((item, i) => {
-          return `<a class="playlist">${i}</a>`;
+          return `<a class="playlist" data-playlistid=${item}>
+                    <img src=../../../../../images/playlist-${i}.png />
+                  </a>`;
         })
-        .join("")}
+        .join("")} 
+      </div>
+      <div class="cabinet-container">
+      ${arr
+        .slice(6, 9)
+        .map((item, i) => {
+          return `<a class="playlist" data-playlistid=${item}>
+                      <img src=../../../../../images/playlist-${i}.png />
+                    </a>`;
+        })
+        .join("")} 
+      </div>
+      <div class="cabinet-container">
+      ${arr
+        .slice(9, 12)
+        .map((item, i) => {
+          return `<a class="playlist" data-playlistid=${item}>
+                      <img src=../../../../../images/playlist-${i}.png />
+                    </a>`;
+        })
+        .join("")} 
+      </div>
+      <div class="cabinet-container">
+      ${arr
+        .slice(12, 15)
+        .map((item, i) => {
+          return `<a class="playlist" data-playlistid=${item}>
+                      <img src=../../../../../images/playlist-${i}.png />
+                    </a>`;
+        })
+        .join("")} 
+      </div>
     `;
   }
   setEvent() {
     this.$target.addEventListener("click", (e) => {
       e.preventDefault();
       if (e.target.classList.contains("playlist")) {
-        const playlistId = e.target.textContent;
+        const playlistId = e.target.dataset.playlistid;
         window.history.pushState(null, "", `/playlists/${playlistId}`);
-        const { $main } = this.props;
-        new Playlist($main, { $main });
+
+        const { $main, playerSetState } = this.props;
+
+        new Playlist($main, { $main, playerSetState });
       }
     });
   }
