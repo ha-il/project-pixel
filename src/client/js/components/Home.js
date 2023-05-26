66eea62b64bea32cb9e54f09a078bec6009b7dba
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-05-26 18:39:16 +0900

    fix: Add image sources to furniture objects And Add Chart component

--- src/client/js/components/Home.js
+++ src/client/js/components/Home.js
@@ -6,47 +6,84 @@ import Bed from "./furnitures/bed.js";
 import Phone from "./Phone.js";
 import { getCookie } from "../utils/cookie.js";
 
+import Chart from "./Chart.js";
+
 class Home extends Component {
   template() {
     return `
     <div class="home-container">
-      <div id="window" class="object">창문</div>
-      <div id="bed" class="object">침대</div>
-      <div id="cabinet" class="object">보관함</div>
-      <div id="tv" class="object">TV</div>
-      <div id="monitor" class="object">${
+      <img id="window" class="object" src=${
         getCookie("isLoggedIn")
-          ? `${getCookie("loggedInUser").profileName}님 환영합니다.`
-          : "로그인 해주세요"
-      }</div>
-      <div id="desk" class="object">책상</div>
-      <div id="coffee" class="object">커피</div>
-      <div id="chair" class="object">의자</div>
-      <div id="modal" class="object hidden"></div>
-      <div id="phone" class="object">아이폰</div>
-      <div id="phone-icon">
-        <i class="fa-solid fa-mobile-screen-button"></i>
+          ? "../../../../images/window.png"
+          : "../../../../images/window-closed.png"
+      } />
+      <img id="bed" class="object" src="../../../../images/bed.png" />
+      <div id="cabinet" class="object">
+        <div class="cabinet-container"></div>
+        <div class="cabinet-container"></div>
+        <div class="cabinet-container"></div>
+        <div class="cabinet-container"></div>
+        <div class="cabinet-container"></div>
       </div>
+      <img id="tv" class="object" src=${
+        getCookie("isLoggedIn")
+          ? "../../../../images/tv-on.gif"
+          : "../../../../images/tv.png"
+      } />
+      <img id="monitor" class="object" src=${
+        getCookie("isLoggedIn")
+          ? "../../../../images/monitor-on.png"
+          : "../../../../images/monitor.gif"
+      } />
+      <img id="desk" class="object" src="../../../../images/desk.png" />
+      
+      ${
+        getCookie("isLoggedIn")
+          ? `<img id="coffee" class="object" src="../../../../images/coffee.png" />`
+          : ""
+      }
+      <img id="chair" class="object" src=${
+        getCookie("isLoggedIn")
+          ? "../../../../images/chair-on.png"
+          : "../../../../images/chair.png"
+      } />
+      <div id="modal" class="object hidden"></div>
+      <div id="phone" class="object"></div>
+      ${
+        getCookie("isLoggedIn")
+          ? `<div id="phone-icon">
+              <i class="fa-solid fa-mobile-screen-button"></i>
+            </div>`
+          : `<div></div>`
+      }
+      
     </div>
+    <div class="floor ${getCookie("isLoggedIn") ? "" : "floor-dark"}"></div>
     `;
   }
   addComponent() {
-    new Cabinet($("#cabinet"), {
-      $main: this.$target,
-    });
+    if (getCookie("isLoggedIn")) {
+      const { playerSetState } = this.props;
+
+      new Cabinet($("#cabinet"), {
+        $main: this.$target,
+        playerSetState,
+      });
+    }
   }
 
   setEvent() {
     $(".home-container").addEventListener("click", (e) => {
-      if (e.target.id === "monitor") {
+      if (e.target.id === "monitor" || e.target.id === "chair") {
+        if (getCookie("isLoggedIn")) return;
         window.history.pushState(null, "", "/login");
         $("#modal").classList.remove("hidden");
-        new Modal($("#modal"), {
-          renderHome: this.render.bind(this),
-        });
+        new Modal($("#modal"));
       }
       if (e.target.id === "bed") {
-        new Bed();
+        if (getCookie("isLoggedIn")) {
+          new Bed();
+        }
       }
       if (e.target.closest("#phone-icon")) {
         $("#phone").classList.toggle("visible");
@@ -54,7 +91,16 @@ class Home extends Component {
           $main: this.$target,
         });
       }
+      if (e.target.id === "tv") {
+        e.preventDefault();
+        window.history.pushState(null, "", `/playlists/chart`);
+
+        new Chart(this.$target, {
+          $main: this.$target,
+        });
+      }
     });
+
     window.addEventListener("DOMContentLoaded", (event) => {
       if (window.location.pathname === "/login") {
         $("#modal").classList.remove("hidden");
